eae10f7055f24e78c24aeaa03d9e3e0f3124ae81
MedivhGO <lijingqi93@gmail.com>
2020-07-11 20:26:14 +0800

    learn condition_variable

---
+++ condition_variable.cc
@@ -0,0 +1,35 @@
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+
+std::mutex mut;
+std::queue<data_chunk> data_queue;
+std::condition_variable data_cond;
+
+void data_preparation_thread()
+{
+    while (more_data_to_prepare())
+    {
+        data_chunk const data = prepare_data();
+        std::lock_guard<std::mutex> lk(mut);
+        data_queue.push(data);
+        data_cond.notify_one();
+    }
+}
+void data_processing_thread()
+{
+    while(true)
+    {
+        std::unique_lock<std::mutex> lk(mut);
+        data_cond.wait(
+            lk,[]{return !data_queue.empty();}
+        );
+        data_queue.pop();
+        lk.unlock();
+        process(data);
+        if(is_last_chunk(data))
+            break;
+
+
+    }
+}
