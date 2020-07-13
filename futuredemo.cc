54a150b7ab2a96c31f738febe317f2ae3a266950
MedivhGO <lijingqi93@gmail.com>
2020-07-13 09:18:38 +0800

    learn async and future

---
+++ futuredemo.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <future>
+
+int find_the_answer_to_ltuae();
+void do_other_stuff();
+int main()
+{
+    std::future<int> the_answer = std::async(find_the_answer_to_ltuae);
+    do_other_stuff();
+    std::cout << "The answer is " << the_answer.get() << std::endl;
+}
+
