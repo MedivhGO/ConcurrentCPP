8f813dd65addc5995cd6c87b24505ba6324175d6
Lee <lijingqi93@foxmail.com>
2021-08-18 11:33:31 +0800

    Update futuredemo.cc

--- futuredemo.cc
+++ futuredemo.cc
@@ -11,6 +11,7 @@ int main()
 }
 */
 #include <thread>
+// std::promise常配合std::future使用。其作用是在一个线程中保存一个类型的值，可供相绑定的std::future对象在另一个线程中获取。
 int main()
 {
     std::packaged_task<int()> task([]{return 7;});
@@ -19,8 +20,8 @@ int main()
 
     std::future<int> f2 = std::async(std::launch::async,[]{return 8;});
 
-    std::promise<int> p;
-    std::future<int> f3 = p.get_future();
+    std::promise<int> p; // 声明一个promise对象p，其保存的值的类型为int
+    std::future<int> f3 = p.get_future(); // 声明一个std::future对象f3，并通过std::promise的get_future()函数与p绑定
     std::thread([&p]{p.set_value_at_thread_exit(9);}).detach();
 
     std::cout << "Waiting..." << std::flush;
